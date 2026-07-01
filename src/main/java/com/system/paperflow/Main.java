6d20544397c800f883804e1edb886045214fa45c
Daniel Alves <alves.daniel4320@gmail.com>
2026-07-01 19:04:30 -0300

    refactor: remove objetos não usados

--- src/main/java/com/system/paperflow/Main.java
+++ src/main/java/com/system/paperflow/Main.java
@@ -1,16 +1,25 @@
 package com.system.paperflow;
 
+import java.util.function.Supplier;
+
 import com.system.paperflow.presentation.screen.DashboardScreen;
 import com.system.paperflow.presentation.screen.LoginScreen;
 import com.system.paperflow.presentation.screen.RegisterScreen;
+import com.system.paperflow.presentation.ui.Screen;
 import com.system.paperflow.presentation.ui.ScreenUtils;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ScreenUtils.register("login", () -> new LoginScreen(() -> ScreenUtils.navigateTo("register")));
-        ScreenUtils.register("register", () -> new RegisterScreen(() -> ScreenUtils.navigateTo("login")));
+        Runnable onNavigateToLogin = () -> ScreenUtils.navigateTo("login");
+        Runnable onNavigateToRegister = () -> ScreenUtils.navigateTo("register");
+
+        Supplier<Screen> loginSupplier = () -> new LoginScreen(onNavigateToRegister);
+        Supplier<Screen> registerSupplier = () -> new RegisterScreen(onNavigateToLogin);
+
+        ScreenUtils.register("login", loginSupplier);
+        ScreenUtils.register("register", registerSupplier);
         ScreenUtils.register("dashboard", DashboardScreen::new);
         ScreenUtils.start("register");
         
